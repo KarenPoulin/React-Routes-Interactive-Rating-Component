9f9ae64d18cf8a089e14ab49652b38c04433e3c2
karen102_14 <karen102_14@hotmail.com>
2023-08-27 23:12:47 -0400

    added backgroudn to success route

--- src/Components/Success/Success.jsx
+++ src/Components/Success/Success.jsx
@@ -7,7 +7,7 @@ import { useParams } from 'react-router-dom';
 const Success = () => {
     const params = useParams();
   return (
-    <Container>
+    <Container className="backgroundContainer">
         <Text as='p'>You selected out <span>{params.rating}</span> of 5</Text>
         <Title>Thank you!</Title>
         <Text as='p'>We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</Text>
